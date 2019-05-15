92220590bdf665b7fe9254529baf612852a92383
haluha1 <votrong_trung999@yahoo.com.vn>
2019-05-15 15:27:07 +0700

    Add Scan Event

--- Checkup/app/src/main/java/com/example/checkup/MainActivity.java
+++ Checkup/app/src/main/java/com/example/checkup/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,6 +21,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.checkup.Model.person;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -171,9 +174,33 @@ public class MainActivity extends AppCompatActivity {
         ButtCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scan your code");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
 
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+            if(result.getContents() == null) {
+                Toast.makeText(this, "You cancel the scanning", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "You have successfully scanned the code", Toast.LENGTH_SHORT).show();
+                idnamba.setText(result.getContents());
+            }
+
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
     public person readPerson(String ID)
     {
